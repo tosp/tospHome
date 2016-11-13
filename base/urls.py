f8157fa550ba1ddee0f58369f2d23531590a9f82
erikiado <erikiado@gmail.com>
2016-11-13 12:09:43 -0600

    Se agrega las templates de prueba para floralint

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from .views import home, base_files
+from .views import home, base_files, homeAccess, homeNoAccess
 
 urlpatterns = [
     url(r'^$', home, name='home'),
+    url(r'^not-access/', homeNoAccess, name='homeNA'),
+    url(r'^access/', homeAccess, name='homeA'),
     url(r'^(?P<filename>(robots.txt)|(humans.txt))$', base_files, name='base_files'),
 ]
--- base/views.py
+++ base/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import render
 def home(request):
     return render(request, "base/home.html")
 
+def homeAccess(request):
+    return render(request, "base/home_access.html")
+
+def homeNoAccess(request):
+    return render(request, "base/home_no_access.html")
+
 
 def base_files(request, filename):
     location = "base/" + filename
